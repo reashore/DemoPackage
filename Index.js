5217779c377c5d4f8e332f67775293af68f9df31
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-12-05 13:46:29 -0800

    Initial commit

---
+++ Index.js
@@ -0,0 +1,18 @@
+
+class Math {
+    static add(x, y) {
+        return x + y;
+    }
+
+    static sub(x, y) {
+        return x - y;
+    }
+
+    static mul(x, y) {
+        return x * y;
+    }
+    
+    static div(x, y) {
+        return x / y;
+    }
+}
